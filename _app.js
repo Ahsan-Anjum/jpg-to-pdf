23f5dbe967bbe44d9f5f7c36615d1c8e5f7d5f27
Ahsan Anjum <115942287+Ahsan-Anjum@users.noreply.github.com>
2024-08-06 18:50:17 +0500

    Update _app.js

--- _app.js
+++ _app.js
@@ -1,13 +1,1 @@
-import { Analytics } from "@vercel/analytics/react";
-import "../styles/globals.css"; // Import your global styles if you have any
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
-}
-
-export default MyApp;
+import { SpeedInsights } from "@vercel/speed-insights/next"
